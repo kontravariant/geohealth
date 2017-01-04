e58c60cd3091e535d18386b46896459017666ed3
tylerkontra <tylerkontra551@gmail.com>
2017-01-04 00:36:23 -0500

    cleaned up old scripts, ignred selenium geckodrivers

--- src/munge/new_income_test.R
+++
@@ -1,3 +0,0 @@
-map = read_csv("project/data/country_map.csv")
-coded = read_csv("project/data/intermediate/country/income_codes.csv")
-dif = setdiff(map$`ISO Alpha3`,coded$`1`);print(dif)
--- src/munge/test_load.py
+++
@@ -1,6 +0,0 @@
-import pickle, os
-datadir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../', 'data/'))
-cwd = os.path.join(datadir, 'processed/')
-
-countryframes = pickle.load(open(os.path.join(cwd,'countries.pkl'), 'rb'))
-print(countryframes['SYC'])
